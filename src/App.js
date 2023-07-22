deb89921ccbeaa5d7a271ed6f64f744467aa6870
importqkk <import_qkk@naver.com>
2023-07-22 21:15:37 +0900

    리액트, 컴포넌트, props 정의

--- src/App.js
+++ src/App.js
@@ -14,19 +14,26 @@ import './App.css';
 function Counter(props) {
     console.log(props);
     return <div>
-                <h1>카운터</h1>
-                <button>+</button> 0
+                <h1>{props.title}</h1>
+                <button>+</button> {props.initValue}
             </div>
 }
 
 function App() {
     return (
-        /* 
+        /*
             여러 줄에 걸쳐 생성된 기능을 하나의 태그로 생성할 수 있음
             -> 재사용, 가독성 높아짐 
+
+            사용자 정의 태그 안의 설정들(title, initValue 등)은 props다.
+            - props: 입력값
+            - 데이터 타입
+                -> 따옴표 안에 적으면 문자열로 취급
+                -> 숫자로 다루고 싶다면 따옴표가 아니라 {} 사용 (문자를 {} 사이에 넣으면 오류남)
         */
         <div>
-            <Counter title="불면증 카운터"></Counter>
+            <Counter title="불면증 카운터" initValue="10"></Counter>
+            <Counter title="손님 카운터" initValue={20}></Counter>
         </div>
     );
 }
